aa76945ad0f654055d8801ff8016a9ab089863e4
liquidsteel49 <alfredmaldonadoiii@gmail.com>
2020-10-27 01:08:34 -0400

    added inline style; need apply internal style

--- solutions/day-03/30-days-of-react/src/index.js
+++ solutions/day-03/30-days-of-react/src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// index.js
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const headerStyles = {
+  backgroundColor: '#61DBFB',
+  fontFamily: 'Helvetica Neue',
+  padding: 25,
+  lineHeight: 1.5,
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// JSX element, header
+const header = (
+  <header style={headerStyles}>
+    <div className='header-wrapper'>
+      <h1>Welcome to 30 Days Of React</h1>
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+      <p>Asabeneh Yetayeh</p>
+      <small>Oct 2, 2020</small>
+    </div>
+  </header>
+)
+
+// JSX element, main
+const mainStyles = {
+  backgroundColor: '#F3F0F5',
+}
+const main = (
+  <main style={mainStyles}>
+    <p>Prerequisite to get started react.js:</p>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
+  </main>
+)
+
+const footerStyles = {
+  backgroundColor: '#61DBFB',
+}
+// JSX element, footer
+const footer = (
+  <footer style={footerStyles}>
+    <p>Copyright 2020</p>
+  </footer>
+)
+
+// JSX element, app
+const app = (
+  <div className='app'>
+    {header}
+    {main}
+    {footer}
+  </div>
+)
+
+const rootElement = document.getElementById('root')
+// we render the JSX element using the ReactDOM package
+ReactDOM.render(app, rootElement)
